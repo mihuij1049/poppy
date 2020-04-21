1d83b2d4d54d02ed9a0ab7cfcebb477e4b40860b
Jungwoo <jwher523@gmail.com>
2020-04-22 00:03:10 +0900

    [허정우] Points 기본 모델 생성

---
+++ src/main/java/kr/co/poppy/model/Points.java
@@ -0,0 +1,24 @@
+package kr.co.poppy.model;
+
+import lombok.Data;
+
+@Data
+public class Points {
+	// 적립금 일련번호
+	private int pointno;
+	// 가용 적립금
+	private Integer avpoint;
+	// 미가용 적립금
+	private Integer napoint;
+	// 포인트 적립내용
+	private char pointtype;
+	// 등록일시
+	private String regdate;
+	// 최종변경일시
+	private String editdate;
+	// 외부 테이블 참조 
+	private int memno;
+	private int orderno;
+	
+	// 페이지 관련 static 변수 미정
+}
