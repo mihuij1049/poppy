587e7d51626dae49b83b7d303cc9edfc8960ebfc
cho <yyaa9650@gmail.com>
2020-04-22 23:42:54 +0900

    [조대철]

--- src/main/java/kr/co/poppy/serviceImpl/OrderdetailServiceImpl.java
+++ src/main/java/kr/co/poppy/serviceImpl/OrderdetailServiceImpl.java
@@ -6,54 +6,53 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import kr.co.poppy.model.Address;
-import kr.co.poppy.service.AddressService;
+import kr.co.poppy.model.Orderdetail;
+import kr.co.poppy.service.OrderdetailService;
 import lombok.extern.slf4j.Slf4j;
 
-/** 주소 데이터 관리 기능을 제공하기 위한 Service 계층에 대한 구현체 */
+/** 상세주문 데이터 관리 기능을 제공하기 위한 Service 계층에 대한 구현체 */
 @Service
 @Slf4j
-public class AddressServiceImpl implements AddressService {
+public class OrderdetailServiceImpl implements OrderdetailService {
 	
 	/** MyBatis */
 	@Autowired
 	SqlSession sqlSession;
 
 	@Override
-	public Address getAddressItem(Address input) throws Exception {
+	public Orderdetail getOrderdetailItem(Orderdetail input) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<Address> getAddressList(Address input) throws Exception {
+	public List<Orderdetail> getOrderdetailList(Orderdetail input) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public int getAddressCount(Address input) throws Exception {
+	public int getOrderdetailCount(Orderdetail input) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public int addAddress(Address input) throws Exception {
+	public int addOrderdetail(Orderdetail input) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public int editAddress(Address input) throws Exception {
+	public int editOrderdetail(Orderdetail input) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public int deleteAddress(Address input) throws Exception {
+	public int deleteOrderdetail(Orderdetail input) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
 
 }
--- src/main/java/kr/co/poppy/serviceImpl/OrdersServiceImpl.java
+++ src/main/java/kr/co/poppy/serviceImpl/OrdersServiceImpl.java
@@ -6,54 +6,53 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import kr.co.poppy.model.Address;
-import kr.co.poppy.service.AddressService;
+import kr.co.poppy.model.Orders;
+import kr.co.poppy.service.OrdersService;
 import lombok.extern.slf4j.Slf4j;
 
-/** 주소 데이터 관리 기능을 제공하기 위한 Service 계층에 대한 구현체 */
+/** 주문 데이터 관리 기능을 제공하기 위한 Service 계층에 대한 구현체 */
 @Service
 @Slf4j
-public class AddressServiceImpl implements AddressService {
+public class OrdersServiceImpl implements OrdersService {
 	
 	/** MyBatis */
 	@Autowired
 	SqlSession sqlSession;
 
 	@Override
-	public Address getAddressItem(Address input) throws Exception {
+	public Orders getOrdersItem(Orders input) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<Address> getAddressList(Address input) throws Exception {
+	public List<Orders> getOrdersList(Orders input) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public int getAddressCount(Address input) throws Exception {
+	public int getOrdersCount(Orders input) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public int addAddress(Address input) throws Exception {
+	public int addOrders(Orders input) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public int editAddress(Address input) throws Exception {
+	public int editOrders(Orders input) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public int deleteAddress(Address input) throws Exception {
+	public int deleteOrders(Orders input) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
 
 }
