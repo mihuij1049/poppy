60037a3cc6bf790ad1be922c4a3bfe1704d31eec
ehye121 <jp1128eun@gmail.com>
2020-04-22 20:42:46 +0900

    [기은혜]bbs, imgs beans 완성

---
+++ src/main/java/kr/co/poppy/model/Imgs.java
@@ -0,0 +1,28 @@
+package kr.co.poppy.model;
+
+import lombok.Data;
+
+@Data
+public class Imgs {
+	
+	private int imgsno;
+	
+	private String imgname;
+	
+	private String imgext;
+	
+	private String imgpath;
+	
+	private int imgsize;
+	
+	private String imgtype;
+	
+	private String regdate;
+	
+	private String editdate;
+	
+	private int goodsno;
+	
+	private int bbsno;
+
+}
---
+++ src/main/java/kr/co/poppy/model/bbs.java
@@ -0,0 +1,32 @@
+package kr.co.poppy.model;
+
+import lombok.Data;
+
+@Data
+public class bbs {
+	
+	private int bbsno;
+	
+	private String bbstype;
+	
+	private String bbstitle;
+	
+	private String bbscontent;
+	
+	private boolean qnasec;
+	
+	private String qnapw;
+	
+	private String rvlike;
+	
+	private String regdate;
+	
+	private String editdate;
+	
+	private int memno;
+	
+	private int goodsno;
+	
+	
+
+}
