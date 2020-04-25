4b60cbed39460e031933a1fd0e489845fbad111b
Jungwoo <jwher523@gmail.com>
2020-04-25 18:48:41 +0900

    [허정우] 오류수정 AgreeMapper, PayController

--- src/main/java/kr/co/poppy/controllers/PayController.java
+++ src/main/java/kr/co/poppy/controllers/PayController.java
@@ -68,39 +68,4 @@ public class PayController {
 		return new ModelAndView("pay/orderform");
 	}
 	
-	/** 목록페이지 */
-	@RequestMapping(value = "/pay/orderform.do", method = RequestMethod.POST)
-	public ModelAndView addrList(Model model,
-			@RequestParam(value="memno", defaultValue="") int memno) {
-		
-		/** 유효성 검사 */
-		// 이 값이 존재하지 않는다면 데이터 조회가 불가능하므로 반드시 필수값으로 처리해야 한다.
-		if (memno == 0) {
-			return webHelper.redirect(null, "회원번호가 없습니다.");
-		}
-		
-		/** 데이터 조회하기 */
-		// 데이터 조회에 필요한 조건값을  Beans에 저장하기
-		Address input = new Address();
-		input.setMemno(memno);
-		
-		// 조회결과를 저장할 객체 선언
-		List<Address> output = null;
-		
-		try {
-			// 데이터 조회
-			output = addressService.getAddressList(input); 
-		} catch (Exception e) {
-			return webHelper.redirect(null, e.getLocalizedMessage());
-		}
-		
-		/** View 처리 */
-		model.addAttribute("output", output);
-		
-		String viewPath = "pay/orderform";
-		return new ModelAndView(viewPath);
-	}
-	
-	
-	
 }
