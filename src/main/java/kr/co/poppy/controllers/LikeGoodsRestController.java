7fd9219adeef6c62515779411d1c0727db6599b7
Jungwoo <jwher523@gmail.com>
2020-05-10 02:07:00 +0900

    [허정우] 관심상품(좋아요) 게시판 구현중(개별/선택/전체삭제완료)

--- src/main/java/kr/co/poppy/controllers/LikeGoodsRestController.java
+++ src/main/java/kr/co/poppy/controllers/LikeGoodsRestController.java
@@ -1,11 +1,16 @@
 package kr.co.poppy.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.poppy.helper.RegexHelper;
@@ -17,43 +22,73 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class LikeGoodsRestController {
-	
+
 	/** WebHelper 주입 */
-	@Autowired WebHelper webHelper;
-	
+	@Autowired
+	WebHelper webHelper;
+
 	/** RegexHelper 주입 */
-	@Autowired RegexHelper regexHelper;
-	
+	@Autowired
+	RegexHelper regexHelper;
+
 	/** Service 구현체 주입 */
-	@Autowired HeartService heartService;
-	
-	/** 삭제 처리 */
-	@RequestMapping(value = "/myInfo", method=RequestMethod.DELETE)
-	public Map<String, Object> delete_item() {
-		/** 1) 필요한 변수값 생성  */
+	@Autowired
+	HeartService heartService;
+
+	/** 삭제 처리 
+	 * @param DelList */
+	@RequestMapping(value = "/myInfo/del_item", method = RequestMethod.DELETE)
+	public Map<String, Object> delete_item()  {
+		
 		// 삭제할 대상에 대한 PK 값 얻기
 		int heartno = webHelper.getInt("heartno");
-		
-		// 이 값이 존재하지 않는다면 데이터 삭제가 불가능하므로 반드시 필수값 처리
-		if (heartno==0) {
-			return webHelper.getJsonWarning("관심상품이 존재하지 않습니다.");
-		}
-		
-		/** 2) 데이터 삭제하기  */
-		// 데이터 삭제에 필요한 PK값을 Beans에 담기
-		Heart input = new Heart();
-		input.setHeartno(heartno);
-		
-		try {
-			heartService.deleteHeart(input);
-		} catch (Exception e) {
-			return webHelper.getJsonError(e.getLocalizedMessage());
+
+		/** 1) 요청받은 파라미터가 heartno 라면? */
+		if (heartno != 0) {
+
+			/** 2) 데이터 삭제하기 */
+			// 데이터 삭제에 필요한 PK값을 Beans에 담기
+			Heart input = new Heart();
+			input.setHeartno(heartno);
+
+			try {
+				heartService.deleteHeart(input);
+			} catch (Exception e) {
+				return webHelper.getJsonError(e.getLocalizedMessage());
+			}
+
 		}
 		
 		/** 3) 결과를 확인하기 위한 JSON 출력 */
 		// 확인할 대상이 삭제된 결과값만 OK로 전달
-		
 		return webHelper.getJsonData();
+	}
+	@RequestMapping(value = "/myInfo/del_list", method = RequestMethod.DELETE)
+	public Map<String, Object> delete_list(@RequestParam(value="delList[]") List<Integer> delList)  {
 		
+		/** 2) 요청받은 파라미터가 delList 라면? */
+		
+		if (delList!=null) {
+			List<Heart> heartList = new ArrayList<Heart>();
+			
+			for (int i=0; i<delList.size();i++) {
+				Heart temp = new Heart();
+				temp.setHeartno(delList.get(i));
+				heartList.add(temp);
+			}
+			
+			Map<String, Object> input = new HashMap<String, Object>();
+			input.put("input", heartList);
+			
+			try {
+				heartService.deleteHeartList(input);
+			} catch (Exception e) {
+				return webHelper.getJsonError(e.getLocalizedMessage());
+			}
+		}
+		
+		/** 3) 결과를 확인하기 위한 JSON 출력 */
+		// 확인할 대상이 삭제된 결과값만 OK로 전달
+		return webHelper.getJsonData();
 	}
 }
